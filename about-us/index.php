e0ff11fa60238ae27dfff92fc39063f4508931d9
Nataho <hashimotonataho@gmail.com>
2026-09-06 18:01:33 +0800

    0.0.5
    added placeholders for about-us/index.php. developer will add more in the future. this just serves as a checkpoint and let the developer know what to work on next

--- about-us/index.php
+++ about-us/index.php
@@ -16,8 +16,6 @@ require_once '../config.php';
     
 
     <!-- hero -->
-    <!-- FIXME -->
-    <!-- Add Middle Text -->
     
     <section class="about-us-image-container">
         <img src="<?=BASE_URL?>assets/images/aboutus1.jpeg" alt="aboutus1">
@@ -29,6 +27,30 @@ require_once '../config.php';
             <p>Truth is a commodity that we’ve been trading in since day one. We’re here to help you find your truth within your skincare routine. That’s true love. That’s true beauty. Applied daily.</p>
         </div>
     </section>
+
+    <!-- Mission -->
+    <section class="section-wrapper">
+        <div class="center">
+            <h1>Mission</h1>
+            <p>Estrera Botanicals formulates clean, 100% plant-based
+                body care to heal and protect skin using organic
+                ingredients. Its mission is to eliminate synthetics, make
+                holistic wellness accessible, and promote sustainable beauty</p>
+        </div>
+    </section>
+
+    <!-- idk if apilon ba ni lol -->
+    <!-- <section class="section-wrapper">
+        <h1>Headquarters</h1>
+        <p>Estrera Botanicals does not have a</p>
+    </section> -->
+
+    <section class="section-wrapper">
+        <!-- picture ni owen diri dapita -->
+        <h1>Our Truth-Seeking Founder</h1>
+        <p>Owen Estrera</p>
+    </section>
+
 
     <!-- footer -->
     <?php require ROOT_DIR . 'components/footer.php'?>
